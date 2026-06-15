2e0a9952ea4a5f5117e8de5734e42c8477b04f90
rocioconejeros <rocio.conejeros@itscordoba.edu.ar>
2026-06-15 14:08:09 -0300

    ultima

--- tp1fastapi/main.py
+++ tp1fastapi/main.py
@@ -5,14 +5,14 @@ from routers import usuarios, productos
 
 app = FastAPI()
 app.include_router(usuarios.router)
-# Incluyo el router de usuarios sin dependencias, ya que no requiere autenticación
+# Incluyo el router de usuarios sin dependencias
 
 app.include_router(
     productos.router,
     dependencies=[Depends(verify_api_token)] 
 )
 
-# Incluyo el router de productos con la dependencia de verificación de token, lo que signifi
-# ca que todos los endpoints de productos requerirán un token válido para acceder
+# Incluyo el router de productos con la dependencia de verificación de token 
+# los endpoints de productos pidan un token para acceder
 
 
